0cc26edd0e704556c5d2600fc04521e2a9f713f7
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-07-25 18:29:24 -0500

    Added prisma

--- pages/form.tsx
+++ pages/form.tsx
@@ -33,7 +33,7 @@ const Form: NextPage = () => {
                 </div>
                 <div className="flex items-center justify-between">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-                        type="button">
+                        type="submit">
                         Submit
                     </button>
                 </div>
