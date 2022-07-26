43f7f6045ffb27c1c578f2219748dee9c4b7a293
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-07-25 22:33:06 -0500

    Added seperate lists

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
 
 const List: NextPage = ({ideas}: any) => {
+  const notstarted = [], inprogress = [], completed = [];
+  
+  for (let i in ideas) {
+    if (ideas[i].status === 'Not Started') notstarted.push(ideas[i]);
+    else if (ideas[i].status === 'In Progress') inprogress.push(ideas[i]);
+    else if (ideas[i].status === 'Completed') completed.push(ideas[i]);
+  }
+
   return (
     <div className="flex items-center justify-center h-screen bg-gray-400">
       <ul>
-        {ideas.map((idea: any) => (
+        {notstarted.map((idea: any) => (
           <li key={idea.id} className="flex">
             <p className="mx-4">{idea.title}</p>
             <p className="mx-4">{idea.description}</p>
---
+++ pages/wheel.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Wheel: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-400">
+        <svg height="100" width="100">
+            <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="3" fill="red" />
+        </svg> 
+    </div>
+  )
+}
+
+export default Wheel
