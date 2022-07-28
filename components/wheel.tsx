c831fbb949df96c259d19afe6fcaf1a56235f9a1
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-07-27 22:52:52 -0500

    Death by svg

--- components/wheel.tsx
+++ components/wheel.tsx
@@ -3,8 +3,10 @@ import type { NextPage } from 'next'
 const Wheel: NextPage = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-400">
-        <svg height="100" width="100" className='spinning'>
-            <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="3" fill="red" />
+        <svg height="500" width="500" className='spinning'>
+            <g stroke="black" strokeWidth="3">
+              <path d="M 250 250 L 250 50 391.42 108.57 250 250 Z"/>
+            </g>
         </svg> 
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next'
 import InProgress from '../components/inProgress'
+import Wheel from '../components/wheel'
 import Form from './form'
 
 const Home: NextPage = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-400">
-      <Form />
-      <InProgress />
+      <Wheel />
     </div>
   )
 }
